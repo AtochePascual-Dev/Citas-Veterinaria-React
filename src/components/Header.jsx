26c4fcb6f1a61b37652c39d47b1034e2b96c66a5
Atoche Pascual <jmatochepascual@gmail.com>
2023-08-01 14:25:55 -1000

    [FEATURE] header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <h1 className="mb-12 text-5xl font-black text-center text-gray-900">
+      Seguimiento Pacientes
+      <span className="block text-indigo-600">Veterinaria</span>
+    </h1>
+  )
+}
+
+export default Header
