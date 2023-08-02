8c903d73eac9b3691389518b38e96897065ae14f
Atoche Pascual <jmatochepascual@gmail.com>
2023-08-01 15:00:43 -1000

    [FEATURE] error component

---
+++ src/components/Error.jsx
@@ -0,0 +1,7 @@
+const Error = () => {
+  return (
+    <div className="p-3 mb-5 font-bold uppercase text-center text-white bg-red-800">todos los campos son obligatorios</div>
+  )
+}
+
+export default Error
